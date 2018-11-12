67d236593b01fbdfc48090c14429ba522d5a39fc
Petrik Lajos Szg <info@petrik.hu>
2018-11-12 15:20:27 +0100

    Kesz a feladat.

---
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace RegisztracioAlkalmazas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.hobbiText = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Ferfi = new System.Windows.Forms.RadioButton();
+            this.No = new System.Windows.Forms.RadioButton();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.hobbiList = new System.Windows.Forms.ListBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameText
+            // 
+            this.nameText.Location = new System.Drawing.Point(93, 15);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(200, 20);
+            this.nameText.TabIndex = 0;
+            // 
+            // hobbiText
+            // 
+            this.hobbiText.Location = new System.Drawing.Point(334, 174);
+            this.hobbiText.Name = "hobbiText";
+            this.hobbiText.Size = new System.Drawing.Size(100, 20);
+            this.hobbiText.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(93, 41);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // Ferfi
+            // 
+            this.Ferfi.AutoSize = true;
+            this.Ferfi.Location = new System.Drawing.Point(130, 81);
+            this.Ferfi.Name = "Ferfi";
+            this.Ferfi.Size = new System.Drawing.Size(31, 17);
+            this.Ferfi.TabIndex = 4;
+            this.Ferfi.TabStop = true;
+            this.Ferfi.Text = "F";
+            this.Ferfi.UseVisualStyleBackColor = true;
+            // 
+            // No
+            // 
+            this.No.AutoSize = true;
+            this.No.Location = new System.Drawing.Point(198, 81);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(33, 17);
+            this.No.TabIndex = 5;
+            this.No.TabStop = true;
+            this.No.Text = "N";
+            this.No.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(235, 262);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(93, 34);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Mentés";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(345, 262);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(85, 34);
+            this.loadButton.TabIndex = 7;
+            this.loadButton.Text = "Betöltés";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(334, 200);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 24);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Hozzáad";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // hobbiList
+            // 
+            this.hobbiList.FormattingEnabled = true;
+            this.hobbiList.Location = new System.Drawing.Point(334, 47);
+            this.hobbiList.Name = "hobbiList";
+            this.hobbiList.Size = new System.Drawing.Size(100, 121);
+            this.hobbiList.TabIndex = 9;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(334, 231);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(100, 23);
+            this.removeButton.TabIndex = 10;
+            this.removeButton.Text = "Töröl";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Szül. dátum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nem:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Kedvenc hobbi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(267, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Új Hobbi:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 308);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.hobbiList);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Ferfi);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.hobbiText);
+            this.Controls.Add(this.nameText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.TextBox hobbiText;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton Ferfi;
+        private System.Windows.Forms.RadioButton No;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox hobbiList;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RegisztracioAlkalmazas/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegisztracioAlkalmazas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (nameText.Text != null && !nameText.Text.Equals("") && dateTimePicker1.Text != null && hobbiList.SelectedItem != null)
+            {
+                StreamWriter fw = new StreamWriter("data.txt");
+
+                fw.WriteLine(nameText.Text);
+                fw.WriteLine(dateTimePicker1.Text);
+                if (Ferfi.Checked == true)
+                {
+                    fw.WriteLine("Ferfi");
+                }
+                if (No.Checked == true)
+                {
+                    fw.WriteLine("No");
+                }
+                fw.WriteLine(hobbiList.SelectedItem);
+                fw.Close();
+
+                MessageBox.Show("Mentés sikeres.");
+            }
+            else
+            {
+                MessageBox.Show("gatya");
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            hobbiList.Items.Add(hobbiText.Text);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (hobbiList.SelectedItem != null)
+            {
+                hobbiList.Items.Remove(hobbiList.SelectedItem);
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("data.txt");
+
+                nameText.Text = sr.ReadLine();
+                sr.ReadLine();
+                String nem = sr.ReadLine();
+                if (nem.Equals("Ferfi"))
+                {
+                    Ferfi.Checked = true;
+                }
+                else
+                {
+                    No.Checked = true;
+                }
+                hobbiList.Items.Clear();
+                hobbiList.Items.Add(sr.ReadLine());
+
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+            
+        }
+    }
+}
